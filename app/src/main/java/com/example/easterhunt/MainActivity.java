cc488437b42b0e9472d1b53fa6ac58320d6e80a3
Jake Craven <jake.craven.mail@gmail.com>
2021-02-25 23:37:38 +0000

    Added Location permission check and request

--- app/src/main/java/com/example/easterhunt/MainActivity.java
+++ app/src/main/java/com/example/easterhunt/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -12,10 +13,14 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import static java.lang.Thread.sleep;
 
 public class MainActivity extends AppCompatActivity {
     MyCurrentLoctionListener CurLoc;
     LocationManager locationManager;
+    private final int locationRequestCode = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,16 @@ public class MainActivity extends AppCompatActivity {
         TextView TestDialog = findViewById(R.id.testTextBox);
         CurLoc = new MyCurrentLoctionListener(TestDialog);
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        permissionDependencies();
+    }
+
+    private void requestAllPermisions() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                locationRequestCode);
+    }
+
+    private void permissionDependencies() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             requestAllPermisions();
             return;
@@ -31,7 +46,16 @@ public class MainActivity extends AppCompatActivity {
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, CurLoc);
     }
 
-    private void requestAllPermisions() {
-
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        if(requestCode == locationRequestCode){
+            for(int result : grantResults){
+                if(result == PackageManager.PERMISSION_DENIED){
+                    Toast.makeText(this.getApplicationContext(),getString(R.string.permissions_explained),Toast.LENGTH_LONG).show();
+                    
+                    return;
+                }
+            }
+            permissionDependencies();
+        }
     }
 }
